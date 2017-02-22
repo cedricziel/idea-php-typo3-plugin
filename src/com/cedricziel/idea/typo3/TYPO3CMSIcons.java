df371c62dfeeb3cceb03843563b95c9219e70c86
Göran Bodenschatz <coding@46halbe.de>
2017-02-22 23:13:25 +0100

    Add icon for “Navigate to route definition”

--- src/com/cedricziel/idea/typo3/TYPO3CMSIcons.java
+++ src/com/cedricziel/idea/typo3/TYPO3CMSIcons.java
@@ -5,5 +5,5 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface TYPO3CMSIcons {
-    Icon ROUTE_ICON = IconLoader.getIcon("/icons/dist/mimetypes-x-content-link.png");
+    Icon ROUTE_ICON = IconLoader.getIcon("/icons/dist/icon-navigate-route-definition.png");
 }
