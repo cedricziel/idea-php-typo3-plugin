5aff8960f30fd6ff9e2c9de5be749e2f820c0325
Cedric Ziel <cedric@cedric-ziel.com>
2017-02-23 16:57:12 +0100

    Support SVG images in icon provider

--- src/com/cedricziel/idea/typo3/util/IconUtil.java
+++ src/com/cedricziel/idea/typo3/util/IconUtil.java
@@ -3,25 +3,41 @@ package com.cedricziel.idea.typo3.util;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.ui.UIUtil;
+import org.apache.batik.transcoder.TranscoderException;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.apache.batik.transcoder.image.ImageTranscoder;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
 public class IconUtil {
 
     public static Icon createIconFromFile(VirtualFile virtualFile) throws IOException {
-        Image image = ImageIO.read(virtualFile.getInputStream());
+        Image image;
+        if (virtualFile.getExtension().equalsIgnoreCase("svg")) {
+            try {
+                image = IconUtil.svg2png(new File(virtualFile.getPath()));
+            } catch (TranscoderException e) {
+                // invalid svg
+                return null;
+            }
+        } else {
+            image = ImageIO.read(virtualFile.getInputStream());
+        }
         if (image == null) {
             return null;
         }
         Icon icon = IconLoader.getIcon(image);
 
         if (UIUtil.isRetina()) {
-            icon = scaleImage((ImageIcon) icon, 16, 16);
-        } else {
             icon = scaleImage((ImageIcon) icon, 32, 32);
+        } else {
+            icon = scaleImage((ImageIcon) icon, 16, 16);
         }
         return icon;
     }
@@ -41,5 +57,25 @@ public class IconUtil {
         }
 
         return new ImageIcon(icon.getImage().getScaledInstance(nw, nh, Image.SCALE_DEFAULT));
+    }
+
+    public static BufferedImage svg2png(File svgFile) throws IOException, TranscoderException {
+        MyTranscoder transcoder = new MyTranscoder();
+        transcoder.transcode(new TranscoderInput(svgFile.toURL().toString()), null);
+
+        return transcoder.getImage();
+    }
+
+    private static class MyTranscoder extends ImageTranscoder {
+        private BufferedImage image = null;
+        public BufferedImage createImage(int w, int h) {
+            image = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+            return image;
+        }
+        public void writeImage(BufferedImage img, TranscoderOutput out) {
+        }
+        public BufferedImage getImage() {
+            return image;
+        }
     }
 }
