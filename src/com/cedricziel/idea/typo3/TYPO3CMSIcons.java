bebcd3a67ce2a8f81c803ed3153b4cc4beed85e4
Cedric Ziel <cedric@cedric-ziel.com>
2017-02-26 21:22:27 +0100

    Add TYPO3 icon

--- src/com/cedricziel/idea/typo3/TYPO3CMSIcons.java
+++ src/com/cedricziel/idea/typo3/TYPO3CMSIcons.java
@@ -5,6 +5,7 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface TYPO3CMSIcons {
+    Icon TYPO3_ICON = IconLoader.getIcon("/icons/dist/icon-typo3.png");
     Icon ROUTE_ICON = IconLoader.getIcon("/icons/dist/icon-navigate-route-definition.png");
     Icon ICON_NOT_RESOLVED = IconLoader.getIcon("/icons/dist/icon-icon-not-resolved.png");
 }
---
+++ src/icons/TYPO3Icons.java
@@ -0,0 +1,9 @@
+package icons;
+
+import com.cedricziel.idea.typo3.TYPO3CMSIcons;
+
+import javax.swing.*;
+
+public class TYPO3Icons {
+    public static final Icon TYPO3_ICON = TYPO3CMSIcons.TYPO3_ICON;
+}
