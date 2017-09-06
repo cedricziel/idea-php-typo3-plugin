38d28032e5dafb8f2c766d9fb5da0c77fd94fb1a
Cedric Ziel <cedric@cedric-ziel.com>
2017-09-06 20:43:37 +0200

    Fix GeneralUtilityTypeProvider

--- src/com/cedricziel/idea/typo3/provider/GeneralUtilityTypeProvider.java
+++ src/com/cedricziel/idea/typo3/provider/GeneralUtilityTypeProvider.java
@@ -2,11 +2,18 @@ package com.cedricziel.idea.typo3.provider;
 
 import com.cedricziel.idea.typo3.psi.PhpElementsUtil;
 import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
+import com.jetbrains.php.PhpIndex;
 import com.jetbrains.php.lang.psi.elements.MethodReference;
+import com.jetbrains.php.lang.psi.elements.PhpNamedElement;
 import com.jetbrains.php.lang.psi.elements.PhpReference;
 import com.jetbrains.php.lang.psi.resolve.types.PhpType;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.Set;
 
 /**
  * TypeProvider for `GeneralUtility::makeInstance`
@@ -39,10 +46,21 @@ public class GeneralUtilityTypeProvider extends AbstractServiceLocatorTypeProvid
         if (firstParam instanceof PhpReference) {
             PhpReference ref = (PhpReference) firstParam;
             if (ref.getText().toLowerCase().contains("::class")) {
-                return new PhpType().add(methodReference.getSignature() + "%" + ref.getSignature());
+                return  new PhpType().add("#" + this.getKey() + ref.getSignature());
             }
         }
 
         return null;
+    }
+
+    public Collection<? extends PhpNamedElement> getBySignature(String expression, Set<String> visited, int depth, Project project) {
+        if(expression.startsWith("#K#C")) {
+            // PhpStorm9: #K#C\Class\Foo.class
+            if(expression.endsWith(".class")) {
+                expression = StringUtils.stripStart(expression.substring(4, expression.length() - 6), "\\");
+            }
+        }
+
+        return PhpIndex.getInstance(project).getClassesByFQN(expression);
     }
 }
