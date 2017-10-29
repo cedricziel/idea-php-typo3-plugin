c21492c8516f08076b3430fc16d2d91c3bc8c04f
Cedric Ziel <cedric@cedric-ziel.com>
2017-10-29 17:13:09 +0100

    Refactor table-related extraction logic to TableUtil

---
+++ src/main/java/com/cedricziel/idea/typo3/util/TableUtil.java
@@ -0,0 +1,65 @@
+package com.cedricziel.idea.typo3.util;
+
+import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.openapi.fileEditor.impl.LoadTextUtil;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.search.FilenameIndex;
+import com.intellij.psi.search.GlobalSearchScope;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TableUtil {
+
+    public static final String EXT_TABLES_SQL_FILENAME = "ext_tables.sql";
+
+    public static Set<String> getAvailableTableNames(@NotNull Project project) {
+        PsiFile[] extSqlFiles = FilenameIndex.getFilesByName(project, EXT_TABLES_SQL_FILENAME, GlobalSearchScope.allScope(project));
+
+        Set<String> tableNames = new HashSet<>();
+
+        for (PsiFile psiFile : extSqlFiles) {
+
+            if (psiFile != null) {
+
+                CharSequence charSequence = LoadTextUtil.loadText(psiFile.getVirtualFile());
+
+                final Matcher matcher = Pattern
+                        .compile("create\\s+table\\s+(if\\s+not\\s+exists\\s+)?([a-zA-Z_0-9]+)", Pattern.CASE_INSENSITIVE)
+                        .matcher(charSequence);
+
+                try {
+                    while (matcher.find()) {
+                        if (matcher.groupCount() < 2) {
+                            return tableNames;
+                        }
+
+                        tableNames.add(matcher.group(2));
+                    }
+                } catch (IllegalStateException e) {
+                    // no matches
+                }
+            }
+        }
+
+        return tableNames;
+    }
+
+    public static void completeAvailableTableNames(@NotNull Project project, @NotNull CompletionResultSet completionResultSet) {
+        for (String name : TableUtil.getAvailableTableNames(project)) {
+            completionResultSet.addElement(new LookupElement() {
+                @NotNull
+                @Override
+                public String getLookupString() {
+
+                    return name;
+                }
+            });
+        }
+    }
+}
