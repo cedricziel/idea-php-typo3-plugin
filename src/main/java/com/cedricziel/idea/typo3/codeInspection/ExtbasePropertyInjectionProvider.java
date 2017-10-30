a94c0c870c813affda4b4bb341ae9bbf76da87f8
Cedric Ziel <cedric@cedric-ziel.com>
2017-10-30 11:03:21 +0100

    Refactor to one general InspectionToolProvider for the plugin
    Avoids registering inspections one at a time.

--- src/main/java/com/cedricziel/idea/typo3/codeInspection/ExtbasePropertyInjectionProvider.java
+++
@@ -1,9 +0,0 @@
-package com.cedricziel.idea.typo3.codeInspection;
-
-import com.intellij.codeInspection.InspectionToolProvider;
-
-public class ExtbasePropertyInjectionProvider implements InspectionToolProvider {
-    public Class[] getInspectionClasses() {
-        return new Class[]{ExtbasePropertyInjectionInspection.class};
-    }
-}
---
+++ src/main/java/com/cedricziel/idea/typo3/codeInspection/TYPO3InspectionToolProvider.java
@@ -0,0 +1,14 @@
+package com.cedricziel.idea.typo3.codeInspection;
+
+import com.intellij.codeInspection.InspectionToolProvider;
+import org.jetbrains.annotations.NotNull;
+
+public class TYPO3InspectionToolProvider implements InspectionToolProvider {
+    @NotNull
+    public Class[] getInspectionClasses() {
+
+        return new Class[]{
+                ExtbasePropertyInjectionInspection.class,
+        };
+    }
+}
