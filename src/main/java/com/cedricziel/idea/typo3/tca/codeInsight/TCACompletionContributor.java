9ce36271d4a3678f633bf45428c0d56856a1c6d8
Cedric Ziel <cedric@cedric-ziel.com>
2017-10-30 14:33:30 +0100

    Autocomplete renderTypes

--- src/main/java/com/cedricziel/idea/typo3/tca/codeInsight/TCACompletionContributor.java
+++ src/main/java/com/cedricziel/idea/typo3/tca/codeInsight/TCACompletionContributor.java
@@ -1,12 +1,15 @@
 package com.cedricziel.idea.typo3.tca.codeInsight;
 
 import com.cedricziel.idea.typo3.psi.PhpElementsUtil;
+import com.cedricziel.idea.typo3.util.TCAUtil;
 import com.cedricziel.idea.typo3.util.TableUtil;
 import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
 
+import static com.cedricziel.idea.typo3.psi.PhpElementsUtil.extractArrayIndexFromValue;
 import static com.cedricziel.idea.typo3.util.TCAUtil.arrayIndexIsTCATableNameField;
 
 public class TCACompletionContributor extends CompletionContributor {
@@ -24,6 +27,33 @@ public class TCACompletionContributor extends CompletionContributor {
                         PsiElement element = parameters.getPosition();
                         if (arrayIndexIsTCATableNameField(element)) {
                             TableUtil.completeAvailableTableNames(element.getProject(), result);
+                        }
+                    }
+                }
+        );
+
+        /*
+         * Complete 'renderType' fields in TCA
+         */
+        extend(
+                CompletionType.BASIC,
+                PhpElementsUtil.isStringArrayValue(),
+                new CompletionProvider<CompletionParameters>() {
+                    @Override
+                    protected void addCompletions(@NotNull CompletionParameters parameters, ProcessingContext context, @NotNull CompletionResultSet result) {
+                        PsiElement element = parameters.getPosition();
+
+                        String arrayIndex = extractArrayIndexFromValue(element);
+                        if ("renderType".equals(arrayIndex)) {
+                            for(String name: TCAUtil.getAvailableRenderTypes(element)) {
+                                result.addElement(new LookupElement() {
+                                    @NotNull
+                                    @Override
+                                    public String getLookupString() {
+                                        return name;
+                                    }
+                                });
+                            }
                         }
                     }
                 }
