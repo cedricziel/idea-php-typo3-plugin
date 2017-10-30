80406bd018ce6b75f24b744048dfea7078e8885e
Cedric Ziel <cedric@cedric-ziel.com>
2017-10-30 14:49:35 +0100

    Add inspection for available / missing renderTypes

---
+++ src/main/java/com/cedricziel/idea/typo3/codeInspection/MissingRenderTypeInspection.java
@@ -0,0 +1,63 @@
+package com.cedricziel.idea.typo3.codeInspection;
+
+import com.cedricziel.idea.typo3.psi.PhpElementsUtil;
+import com.cedricziel.idea.typo3.util.TCAUtil;
+import com.cedricziel.idea.typo3.util.TableUtil;
+import com.intellij.codeInsight.daemon.GroupNames;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElementVisitor;
+import com.jetbrains.php.lang.inspections.PhpInspection;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+
+import static com.cedricziel.idea.typo3.psi.PhpElementsUtil.extractArrayIndexFromValue;
+
+public class MissingRenderTypeInspection extends PhpInspection {
+    @Nls
+    @NotNull
+    @Override
+    public String getGroupDisplayName() {
+        return GroupNames.BUGS_GROUP_NAME;
+    }
+
+    @NotNull
+    public String getDisplayName() {
+        return "Missing renderType definition";
+    }
+
+    @NotNull
+    public String getShortName() {
+        return "MissingRenderType";
+    }
+
+    @NotNull
+    @Override
+    public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder problemsHolder, boolean b) {
+        return new PhpElementVisitor() {
+            @Override
+            public void visitPhpElement(PhpPsiElement element) {
+
+                boolean isArrayStringValue = PhpElementsUtil.isStringArrayValue().accepts(element);
+                if (!isArrayStringValue) {
+                    return;
+                }
+
+
+                String arrayIndex = extractArrayIndexFromValue(element);
+                if (arrayIndex != null && arrayIndex.equals("renderType")) {
+                    if (element instanceof StringLiteralExpression) {
+                        String tableName = ((StringLiteralExpression) element).getContents();
+                        boolean isValidRenderType = TCAUtil.getAvailableRenderTypes(element).contains(tableName);
+
+                        if (!isValidRenderType) {
+                            problemsHolder.registerProblem(element, "Missing renderType definition");
+                        }
+                    }
+                }
+            }
+        };
+    }
+}
