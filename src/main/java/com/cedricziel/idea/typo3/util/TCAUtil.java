a565e46c0df78c6258f288afa4d1e94e070049a0
Cedric Ziel <cedric@cedric-ziel.com>
2017-10-30 14:18:47 +0100

    Add core column types and renderTypes to TCA Util

--- src/main/java/com/cedricziel/idea/typo3/util/TCAUtil.java
+++ src/main/java/com/cedricziel/idea/typo3/util/TCAUtil.java
@@ -3,6 +3,8 @@ package com.cedricziel.idea.typo3.util;
 import com.intellij.psi.PsiElement;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 import static com.cedricziel.idea.typo3.psi.PhpElementsUtil.extractArrayIndexFromValue;
 
@@ -13,9 +15,48 @@ public class TCAUtil {
             "allowed",
     };
 
+    public static final String[] TCA_CORE_TYPES = {
+            "flex",
+            "group",
+            "imageManipulation",
+            "input",
+            "inline",
+            "none",
+            "passthrough",
+            "radio",
+            "select",
+            "text",
+            "user",
+    };
+
+    public static final String[] TCA_CORE_RENDER_TYPES = {
+            "selectSingle",
+            "selectSingleBox",
+            "selectCheckBox",
+            "selectMultipleSideBySide",
+            "selectTree",
+            "colorpicker",
+            "inputDateTime",
+            "inputLink",
+            "rsaInput",
+            "belayoutwizard",
+            "t3editor",
+            "textTable",
+    };
+
     public static boolean arrayIndexIsTCATableNameField(PsiElement element) {
         String arrayIndex = extractArrayIndexFromValue(element);
 
         return Arrays.asList(TCA_TABLE_FIELDS).contains(arrayIndex);
+    }
+
+    public static Set<String> getAvailableRenderTypes(PsiElement element) {
+
+        return new HashSet<>(Arrays.asList(TCA_CORE_RENDER_TYPES));
+    }
+
+    public static Set<String> getAvailableColumnTypes(PsiElement element) {
+
+        return new HashSet<>(Arrays.asList(TCA_CORE_TYPES));
     }
 }
