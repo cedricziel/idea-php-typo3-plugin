f75ae01efb1d417185e058d6e1cf5523e48000af
Cedric Ziel <cedric@cedric-ziel.com>
2017-11-04 18:19:49 +0100

    Add invalid quantity inspection

--- src/main/java/com/cedricziel/idea/typo3/codeInspection/TYPO3InspectionToolProvider.java
+++ src/main/java/com/cedricziel/idea/typo3/codeInspection/TYPO3InspectionToolProvider.java
@@ -1,5 +1,6 @@
 package com.cedricziel.idea.typo3.codeInspection;
 
+import com.cedricziel.idea.typo3.tca.codeInspection.InvalidQuantityInspection;
 import com.intellij.codeInspection.InspectionToolProvider;
 import org.jetbrains.annotations.NotNull;
 
@@ -12,6 +13,7 @@ public class TYPO3InspectionToolProvider implements InspectionToolProvider {
                 MissingColumnTypeInspection.class,
                 MissingRenderTypeInspection.class,
                 MissingTableInspection.class,
+                InvalidQuantityInspection.class
         };
     }
 }
---
+++ src/main/java/com/cedricziel/idea/typo3/tca/codeInspection/InvalidQuantityInspection.java
@@ -0,0 +1,53 @@
+package com.cedricziel.idea.typo3.tca.codeInspection;
+
+import com.cedricziel.idea.typo3.psi.PhpElementsUtil;
+import com.cedricziel.idea.typo3.util.TCAUtil;
+import com.intellij.codeInsight.daemon.GroupNames;
+import com.intellij.codeInspection.ProblemsHolder;
+import com.intellij.psi.PsiElementVisitor;
+import com.jetbrains.php.lang.inspections.PhpInspection;
+import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
+import static com.cedricziel.idea.typo3.psi.PhpElementsUtil.extractArrayIndexFromValue;
+
+public class InvalidQuantityInspection extends PhpInspection {
+    @Nls
+    @NotNull
+    @Override
+    public String getGroupDisplayName() {
+        return GroupNames.BUGS_GROUP_NAME;
+    }
+
+    @NotNull
+    public String getDisplayName() {
+        return "Config key only accepts integer values";
+    }
+
+    @NotNull
+    @Override
+    public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder problemsHolder, boolean b) {
+        return new PhpElementVisitor() {
+            @Override
+            public void visitPhpElement(PhpPsiElement element) {
+
+                boolean isArrayStringValue = PhpElementsUtil.isStringArrayValue().accepts(element);
+                if (!isArrayStringValue) {
+                    return;
+                }
+
+                String arrayIndex = extractArrayIndexFromValue(element);
+                if (arrayIndex != null && Arrays.asList(TCAUtil.TCA_NUMERIC_CONFIG_KEYS).contains(arrayIndex)) {
+                    if (element instanceof StringLiteralExpression) {
+                        problemsHolder.registerProblem(element, "Config key only accepts integer values");
+                    }
+                }
+            }
+        };
+    }
+}
