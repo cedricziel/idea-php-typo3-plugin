2b7eb646b4973121f8037f6dc67e3b821803454c
Cedric Ziel <cedric@cedric-ziel.com>
2017-11-05 20:42:09 +0100

    Add ConstantMatcherExpression

--- src/main/java/com/cedricziel/idea/typo3/codeInspection/TYPO3InspectionToolProvider.java
+++ src/main/java/com/cedricziel/idea/typo3/codeInspection/TYPO3InspectionToolProvider.java
@@ -2,6 +2,7 @@ package com.cedricziel.idea.typo3.codeInspection;
 
 import com.cedricziel.idea.typo3.extensionScanner.codeInspection.ClassConstantMatcherInspection;
 import com.cedricziel.idea.typo3.extensionScanner.codeInspection.ClassNameMatcherInspection;
+import com.cedricziel.idea.typo3.extensionScanner.codeInspection.ConstantMatcherInspection;
 import com.cedricziel.idea.typo3.tca.codeInspection.InvalidQuantityInspection;
 import com.intellij.codeInspection.InspectionToolProvider;
 import org.jetbrains.annotations.NotNull;
@@ -19,6 +20,7 @@ public class TYPO3InspectionToolProvider implements InspectionToolProvider {
                 // Extension Scanner
                 ClassConstantMatcherInspection.class,
                 ClassNameMatcherInspection.class,
+                ConstantMatcherInspection.class,
         };
     }
 }
