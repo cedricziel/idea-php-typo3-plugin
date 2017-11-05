284eb37fabb094ad9a4a5986b45dd1ece72bb4cf
Cedric Ziel <cedric@cedric-ziel.com>
2017-11-05 20:13:25 +0100

    Add inspections for deprecated class constants and removed classes in TYPO3 v9

--- src/main/java/com/cedricziel/idea/typo3/codeInspection/TYPO3InspectionToolProvider.java
+++ src/main/java/com/cedricziel/idea/typo3/codeInspection/TYPO3InspectionToolProvider.java
@@ -1,5 +1,7 @@
 package com.cedricziel.idea.typo3.codeInspection;
 
+import com.cedricziel.idea.typo3.extensionScanner.codeInspection.ClassConstantMatcherInspection;
+import com.cedricziel.idea.typo3.extensionScanner.codeInspection.ClassNameMatcherInspection;
 import com.cedricziel.idea.typo3.tca.codeInspection.InvalidQuantityInspection;
 import com.intellij.codeInspection.InspectionToolProvider;
 import org.jetbrains.annotations.NotNull;
@@ -13,7 +15,10 @@ public class TYPO3InspectionToolProvider implements InspectionToolProvider {
                 MissingColumnTypeInspection.class,
                 MissingRenderTypeInspection.class,
                 MissingTableInspection.class,
-                InvalidQuantityInspection.class
+                InvalidQuantityInspection.class,
+                // Extension Scanner
+                ClassConstantMatcherInspection.class,
+                ClassNameMatcherInspection.class,
         };
     }
 }
