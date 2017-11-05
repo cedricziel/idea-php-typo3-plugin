ab54ac0547bec0453c2baa3c1bfe0c4576232338
Cedric Ziel <cedric@cedric-ziel.com>
2017-11-05 20:52:02 +0100

    Add GlobalFunctionCallMatcherInspection

--- src/main/java/com/cedricziel/idea/typo3/codeInspection/TYPO3InspectionToolProvider.java
+++ src/main/java/com/cedricziel/idea/typo3/codeInspection/TYPO3InspectionToolProvider.java
@@ -3,6 +3,7 @@ package com.cedricziel.idea.typo3.codeInspection;
 import com.cedricziel.idea.typo3.extensionScanner.codeInspection.ClassConstantMatcherInspection;
 import com.cedricziel.idea.typo3.extensionScanner.codeInspection.ClassNameMatcherInspection;
 import com.cedricziel.idea.typo3.extensionScanner.codeInspection.ConstantMatcherInspection;
+import com.cedricziel.idea.typo3.extensionScanner.codeInspection.FunctionCallMatcherInspection;
 import com.cedricziel.idea.typo3.tca.codeInspection.InvalidQuantityInspection;
 import com.intellij.codeInspection.InspectionToolProvider;
 import org.jetbrains.annotations.NotNull;
@@ -21,6 +22,7 @@ public class TYPO3InspectionToolProvider implements InspectionToolProvider {
                 ClassConstantMatcherInspection.class,
                 ClassNameMatcherInspection.class,
                 ConstantMatcherInspection.class,
+                FunctionCallMatcherInspection.class,
         };
     }
 }
