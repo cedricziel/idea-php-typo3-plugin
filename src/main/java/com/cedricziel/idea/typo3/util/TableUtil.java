6665751cfd2c1fb35d22ab7bd2eba3e3d2617fee
Cedric Ziel <cedric@cedric-ziel.com>
2017-11-15 17:14:49 +0100

    Make TableUtil use a stable manifested index rather than on-the-fly grepping

--- src/main/java/com/cedricziel/idea/typo3/util/TableUtil.java
+++ src/main/java/com/cedricziel/idea/typo3/util/TableUtil.java
@@ -1,100 +1,69 @@
 package com.cedricziel.idea.typo3.util;
 
+import com.cedricziel.idea.typo3.index.TablenameFileIndex;
 import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.codeInsight.lookup.LookupElement;
-import com.intellij.openapi.fileEditor.impl.LoadTextUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.indexing.FileBasedIndex;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class TableUtil {
 
     public static final String EXT_TABLES_SQL_FILENAME = "ext_tables.sql";
 
     public static Set<String> getAvailableTableNames(@NotNull Project project) {
-        PsiFile[] extSqlFiles = getExtTablesSqlFilesInProject(project);
 
-        Set<String> tableNames = new HashSet<>();
-
-        for (PsiFile psiFile : extSqlFiles) {
-
-            if (psiFile != null) {
-
-                CharSequence charSequence = LoadTextUtil.loadText(psiFile.getVirtualFile());
-
-                final Matcher matcher = Pattern
-                        .compile("create\\s+table\\s+(if\\s+not\\s+exists\\s+)?([a-zA-Z_0-9]+)", Pattern.CASE_INSENSITIVE)
-                        .matcher(charSequence);
-
-                try {
-                    while (matcher.find()) {
-                        if (matcher.groupCount() < 2) {
-                            return tableNames;
-                        }
-
-                        tableNames.add(matcher.group(2));
-                    }
-                } catch (IllegalStateException e) {
-                    // no matches
-                }
-            }
-        }
-
-        return tableNames;
+        return new HashSet<>(FileBasedIndex.getInstance().getAllKeys(TablenameFileIndex.KEY, project));
     }
 
     public static PsiElement[] getTableDefinitionElements(@NotNull String tableName, @NotNull Project project) {
 
         PsiFile[] extTablesSqlFilesInProjectContainingTable = getExtTablesSqlFilesInProjectContainingTable(tableName, project);
+        Set<PsiElement> elements = new HashSet<>();
 
-        return Arrays
-                .stream(extTablesSqlFilesInProjectContainingTable)
-                .map(file -> {
-                    CharSequence charSequence = LoadTextUtil.loadText(file.getVirtualFile());
+        PsiManager psiManager = PsiManager.getInstance(project);
 
-                    final Matcher matcher = Pattern
-                            .compile("create\\s+table\\s+(if\\s+not\\s+exists\\s+)?([a-zA-Z_0-9]+)", Pattern.CASE_INSENSITIVE)
-                            .matcher(charSequence);
-                    try {
-                        while (matcher.find()) {
-                            if (matcher.groupCount() < 2) {
-                                continue;
-                            }
+        for (PsiFile virtualFile : extTablesSqlFilesInProjectContainingTable) {
+            FileBasedIndex.getInstance().processValues(TablenameFileIndex.KEY, tableName, virtualFile.getVirtualFile(), (file, value) -> {
 
-                            final String foundTable = matcher.group(2);
-                            if (!foundTable.equals(tableName)) {
-                                continue;
-                            }
-
-                            return file.findElementAt(matcher.end() - 2);
-                        }
-                    } catch (IllegalStateException e) {
-                        // do nothing
+                PsiFile file1 = psiManager.findFile(file);
+                if (file1 != null) {
+                    PsiElement elementAt = file1.findElementAt(value.getEndOffset() - 2);
+                    if (elementAt != null) {
+                        elements.add(elementAt);
                     }
+                }
 
-                    return null;
-                })
-                .filter(Objects::nonNull)
-                .toArray(PsiElement[]::new);
+                return true;
+            }, GlobalSearchScope.allScope(project));
+        }
+
+        return elements.toArray(new PsiElement[elements.size()]);
     }
 
     private static PsiFile[] getExtTablesSqlFilesInProjectContainingTable(@NotNull String tableName, @NotNull Project project) {
+        Set<String> set = new HashSet<>();
+        set.add(tableName);
 
-        return Arrays
-                .stream(getExtTablesSqlFilesInProject(project))
-                .filter(Objects::nonNull)
-                .filter(file -> LoadTextUtil.loadText(file.getVirtualFile()).toString().contains(tableName))
-                .toArray(PsiFile[]::new);
+        Set<PsiFile> files = new HashSet<>();
+        PsiManager psiManager = PsiManager.getInstance(project);
+
+        FileBasedIndex.getInstance().getFilesWithKey(TablenameFileIndex.KEY, set, virtualFile -> {
+
+            files.add(psiManager.findFile(virtualFile));
+
+            return true;
+        }, GlobalSearchScope.allScope(project));
+
+        return files.toArray(new PsiFile[files.size()]);
     }
 
     @NotNull
