71078e6990d8d2870b39598c392c16573868f1d4
Cedric Ziel <cedric@cedric-ziel.com>
2017-11-18 23:58:51 +0100

    Migrate PathResourceGotoDeclarationHandler to use the index

--- src/main/java/com/cedricziel/idea/typo3/index/ResourcePathIndex.java
+++ src/main/java/com/cedricziel/idea/typo3/index/ResourcePathIndex.java
@@ -1,18 +1,49 @@
 package com.cedricziel.idea.typo3.index;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.indexing.*;
 import com.intellij.util.io.EnumeratorStringDescriptor;
 import com.intellij.util.io.KeyDescriptor;
 import gnu.trove.THashMap;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 
 public class ResourcePathIndex extends ScalarIndexExtension<String> {
 
     public static final ID<String, Void> KEY = ID.create("com.cedricziel.idea.typo3.index.resource_path");
+
+    public static Collection<String> getAvailableExtensionResourceFiles(@NotNull Project project) {
+        return FileBasedIndex.getInstance().getAllKeys(ResourcePathIndex.KEY, project);
+    }
+
+    public static boolean projectContainsResourceFile(@NotNull Project project, @NotNull String resourceId) {
+        return FileBasedIndex.getInstance().getAllKeys(ResourcePathIndex.KEY, project).contains(resourceId);
+    }
+
+    public static boolean projectContainsResourceDirectory(@NotNull Project project, @NotNull String resourceId) {
+        return false;
+    }
+
+    public static PsiElement[] findElementsForKey(@NotNull Project project, @NotNull String identifier) {
+        Set<String> keys = new HashSet<>();
+        keys.add(identifier);
+        Set<PsiElement> elements = new HashSet<>();
+
+        FileBasedIndex.getInstance().getFilesWithKey(ResourcePathIndex.KEY, keys, virtualFile -> {
+            elements.add(PsiManager.getInstance(project).findFile(virtualFile));
+
+            return true;
+        }, GlobalSearchScope.allScope(project));
+
+        return elements
+                .stream()
+                .filter(Objects::nonNull)
+                .toArray(PsiElement[]::new);
+    }
 
     @NotNull
     @Override
@@ -59,23 +90,11 @@ public class ResourcePathIndex extends ScalarIndexExtension<String> {
     @NotNull
     @Override
     public FileBasedIndex.InputFilter getInputFilter() {
-        return file -> file.isInLocalFileSystem() && (file.getPath().contains("sysext") ||file.getPath().contains("typo3conf/ext"));
+        return file -> file.isInLocalFileSystem() && (file.getPath().contains("sysext") || file.getPath().contains("typo3conf/ext"));
     }
 
     @Override
     public boolean dependsOnFileContent() {
-        return false;
-    }
-
-    public static Collection<String> getAvailableExtensionResourceFiles(@NotNull Project project) {
-        return FileBasedIndex.getInstance().getAllKeys(ResourcePathIndex.KEY, project);
-    }
-
-    public static boolean projectContainsResourceFile(@NotNull Project project, @NotNull String resourceId) {
-        return FileBasedIndex.getInstance().getAllKeys(ResourcePathIndex.KEY, project).contains(resourceId);
-    }
-
-    public static boolean projectContainsResourceDirectory(@NotNull Project project, @NotNull String resourceId) {
         return false;
     }
 }
