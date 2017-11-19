f710a751f47a75c82947c79bd87a611c354b6732
Cedric Ziel <cedric@cedric-ziel.com>
2017-11-19 01:32:30 +0100

    Correct path resource tests

--- src/test/java/com/cedricziel/idea/typo3/codeInsight/PathResourceCompletionContributorTest.java
+++ src/test/java/com/cedricziel/idea/typo3/codeInsight/PathResourceCompletionContributorTest.java
@@ -1,14 +1,25 @@
 package com.cedricziel.idea.typo3.codeInsight;
 
-import com.cedricziel.idea.typo3.index.ResourcePathIndex;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+import com.jetbrains.php.lang.PhpFileType;
+
+import java.util.List;
 
 public class PathResourceCompletionContributorTest extends LightCodeInsightFixtureTestCase {
     public void testResourcesAreIndexed() {
         myFixture.addFileToProject("typo3conf/ext/foo/bar.php", "");
         myFixture.addFileToProject("typo3/sysext/baz/bar.png", "");
+        myFixture.configureByText(PhpFileType.INSTANCE, "<?php \n" +
+                "echo 'EXT:<caret>';");
+        myFixture.completeBasic();
 
-        assertTrue(ResourcePathIndex.projectContainsResourceFile(myFixture.getProject(), "EXT:foo/bar.php"));
-        assertTrue(ResourcePathIndex.projectContainsResourceFile(myFixture.getProject(), "EXT:baz/bar.png"));
+        List<String> lookupElementStrings = myFixture.getLookupElementStrings();
+
+        if (lookupElementStrings == null) {
+            fail("Could not complete");
+        }
+
+        assertContainsElements(lookupElementStrings, "EXT:foo/bar.php");
+        assertContainsElements(lookupElementStrings, "EXT:baz/bar.png");
     }
 }
--- src/test/java/com/cedricziel/idea/typo3/index/ResourcePathIndexTest.java
+++ src/test/java/com/cedricziel/idea/typo3/index/ResourcePathIndexTest.java
@@ -1,26 +1,14 @@
 package com.cedricziel.idea.typo3.index;
 
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
-import com.jetbrains.php.lang.PhpFileType;
-
-import java.util.List;
 
 public class ResourcePathIndexTest extends LightCodeInsightFixtureTestCase {
     public void testResourcesAreIndexed() {
         myFixture.addFileToProject("typo3conf/ext/foo/bar.php", "");
         myFixture.addFileToProject("typo3/sysext/baz/bar.png", "");
-        myFixture.configureByText(PhpFileType.INSTANCE, "<?php \n" +
-                "echo 'EXT:<caret>';");
-        myFixture.completeBasic();
 
-        List<String> lookupElementStrings = myFixture.getLookupElementStrings();
-
-        if (lookupElementStrings == null) {
-            fail("Could not complete");
-        }
-
-        assertContainsElements(lookupElementStrings, "EXT:foo/bar.php");
-        assertContainsElements(lookupElementStrings, "EXT:baz/bar.png");
+        assertTrue(ResourcePathIndex.projectContainsResourceFile(myFixture.getProject(), "EXT:foo/bar.php"));
+        assertTrue(ResourcePathIndex.projectContainsResourceFile(myFixture.getProject(), "EXT:baz/bar.png"));
     }
 
     public void testInstallerNameUnconventionalResourcePathsAreDetected() {
