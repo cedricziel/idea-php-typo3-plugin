b5e2f452a8c8daa5f3cbcc3ae3845fe056a71254
Cedric Ziel <cedric@cedric-ziel.com>
2017-11-27 20:32:50 +0100

    Add default templates for TYPO3 7-9

--- src/main/resources/fileTemplates/extension_file/7/ViewHelper.php
+++ src/main/resources/fileTemplates/extension_file/7/ViewHelper.php
@@ -4,37 +4,26 @@ namespace {{ namespace }};
 
 use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
 
-class {{ className }} extends AbstractViewHelper
+class {{ className }} extends AbstractViewHelper implements CompilableInterface
 {
     /**
-     * @param string $value The value to output
-     * @return string
+     * @param string $value A string value
      */
     public function render($value = null)
     {
         return static::renderStatic(
             [
-                'value' => $value
+                'value' => $value,
             ],
             $this->buildRenderChildrenClosure(),
             $this->renderingContext
         );
     }
 
-    /**
-     * @param array $arguments
-     * @param \Closure $renderChildrenClosure
-     * @param RenderingContextInterface $renderingContext
-     * @return string
-     */
     public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
     {
-        $value = $arguments['value'];
-        if ($value === null) {
-            return $renderChildrenClosure();
-        } else {
-            return $value;
-        }
+        return $renderChildrenClosure();
     }
 }
