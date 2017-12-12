397c807f7d375cdd7d6dd78c395962260a8822ad
Cedric Ziel <cedric@cedric-ziel.com>
2017-12-12 19:49:55 +0100

    Cleanup RouteAnnotator

--- src/main/java/com/cedricziel/idea/typo3/annotation/RouteAnnotator.java
+++ src/main/java/com/cedricziel/idea/typo3/annotation/RouteAnnotator.java
@@ -1,17 +1,15 @@
 package com.cedricziel.idea.typo3.annotation;
 
 import com.cedricziel.idea.typo3.index.RouteIndex;
-import com.cedricziel.idea.typo3.psi.PhpElementsUtil;
+import com.cedricziel.idea.typo3.routing.RouteReference;
 import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.PsiReference;
 import com.intellij.util.indexing.FileBasedIndex;
-import com.jetbrains.php.lang.psi.elements.ArrayCreationExpression;
-import com.jetbrains.php.lang.psi.elements.MethodReference;
 import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import org.jetbrains.annotations.NotNull;
 
@@ -32,24 +30,14 @@ public class RouteAnnotator implements Annotator {
             return;
         }
 
-        PsiElement methodReference = PsiTreeUtil.getParentOfType(psiElement, MethodReference.class);
-        if (PhpElementsUtil.isMethodWithFirstStringOrFieldReference(methodReference, "getAjaxUrl") && !hasArrayCreationParent(psiElement)) {
-            annotateRoute(psiElement, annotationHolder, value);
-        }
-
-        if (PhpElementsUtil.isMethodWithFirstStringOrFieldReference(methodReference, "buildUriFromRoute") && !hasArrayCreationParent(psiElement)) {
-            annotateRoute(psiElement, annotationHolder, value);
+        for (PsiReference psiReference : literalExpression.getReferences()) {
+            if (psiReference instanceof RouteReference) {
+                annotateRoute(psiElement, annotationHolder, value);
+            }
         }
     }
 
-    private boolean hasArrayCreationParent(@NotNull PsiElement psiElement) {
-        if (psiElement.getParent() == null) {
-            return false;
-        }
-        return psiElement.getParent().getParent() instanceof ArrayCreationExpression;
-    }
-
-    private void annotateRoute(PsiElement psiElement, AnnotationHolder annotationHolder, String value) {
+    private void annotateRoute(@NotNull PsiElement psiElement, @NotNull AnnotationHolder annotationHolder, @NotNull String value) {
         Collection<String> allKeys = FileBasedIndex.getInstance().getAllKeys(RouteIndex.KEY, psiElement.getProject());
 
         if (allKeys.contains(value)) {
