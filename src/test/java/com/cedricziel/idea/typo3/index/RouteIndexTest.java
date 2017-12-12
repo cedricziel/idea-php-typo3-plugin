d3414e26a1acc1a0de2d516a6fc187fca2a3cbef
Cedric Ziel <cedric@cedric-ziel.com>
2017-12-12 21:00:50 +0100

    Fix route indexing

---
+++ src/test/java/com/cedricziel/idea/typo3/index/RouteIndexTest.java
@@ -0,0 +1,48 @@
+package com.cedricziel.idea.typo3.index;
+
+import com.cedricziel.idea.typo3.routing.RouteStub;
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
+
+public class RouteIndexTest extends LightCodeInsightFixtureTestCase {
+    @Override
+    protected String getTestDataPath() {
+        return "testData/com/cedricziel/idea/typo3/index/route";
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        myFixture.addFileToProject("typo3conf/ext/foo/ext_emconf.php", "");
+        myFixture.addFileToProject("typo3conf/ext/bar/ext_emconf.php", "");
+        myFixture.copyFileToProject("Routes.php", "typo3conf/ext/foo/Configuration/Backend/Routes.php");
+        myFixture.copyFileToProject("BackendRoutes.php", "typo3conf/ext/bar/Configuration/Backend/Routes.php");
+    }
+
+    public void testRoutesAreIndexed() {
+        assertRouteExists("xMOD_tximpexp");
+        assertRouteExists("login");
+        assertRouteExists("login_frameset");
+        assertRouteExists("file_newfolder");
+    }
+
+    public void testRouteStubsAreCorrectlyGenerated() {
+        assertRouteStubEquals("file_newfolder", "/file/new", "\\TYPO3\\CMS\\Backend\\Controller\\File\\CreateFolderController", "mainAction", "private");
+        assertRouteStubEquals("tce_db", "/record/commit", "\\TYPO3\\CMS\\Backend\\Controller\\SimpleDataHandlerController", "mainAction", "private");
+        assertRouteStubEquals("login", "/login", "\\TYPO3\\CMS\\Backend\\Controller\\LoginController", "formAction", "public");
+    }
+
+    private void assertRouteStubEquals(String name, String path, String controllerClass, String action, String access) {
+        RouteStub stub = RouteIndex.getRoute(myFixture.getProject(), name).iterator().next();
+
+        assertEquals(name, stub.getName());
+        assertEquals(path, stub.getPath());
+        assertEquals(controllerClass, stub.getController());
+        assertEquals(action, stub.getMethod());
+        assertEquals(access, stub.getAccess());
+    }
+
+    private void assertRouteExists(String routeName) {
+        assertTrue(RouteIndex.hasRoute(myFixture.getProject(), routeName));
+    }
+}
