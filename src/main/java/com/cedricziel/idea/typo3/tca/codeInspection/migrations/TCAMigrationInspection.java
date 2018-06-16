b4766135db63cf20098096336af42f5ce379ab07
Cedric Ziel <cedric@cedric-ziel.com>
2018-06-16 15:04:56 +0200

    Commit missing TCAMigrationInspection

---
+++ src/main/java/com/cedricziel/idea/typo3/tca/codeInspection/migrations/TCAMigrationInspection.java
@@ -0,0 +1,6 @@
+package com.cedricziel.idea.typo3.tca.codeInspection.migrations;
+
+import com.intellij.codeInspection.LocalInspectionTool;
+
+public abstract class TCAMigrationInspection extends LocalInspectionTool {
+}
