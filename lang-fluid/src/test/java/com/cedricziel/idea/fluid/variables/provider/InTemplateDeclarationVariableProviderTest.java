a3bedd45faa4c7ee433f1634584eb605f82faa99
Cedric Ziel <cedric@cedric-ziel.com>
2018-08-07 22:07:40 +0200

    [Fluid] Remove faulty check

--- lang-fluid/src/test/java/com/cedricziel/idea/fluid/variables/provider/InTemplateDeclarationVariableProviderTest.java
+++ lang-fluid/src/test/java/com/cedricziel/idea/fluid/variables/provider/InTemplateDeclarationVariableProviderTest.java
@@ -4,7 +4,7 @@ import com.cedricziel.idea.fluid.AbstractFluidTest;
 
 public class InTemplateDeclarationVariableProviderTest extends AbstractFluidTest {
     public void testVariablesAreProvided() {
-        assertCompletionContains("<f:variable name=\"foo\" value=\"foo\"/>\n{<caret>}", "foo", "bar");
+        assertCompletionContains("<f:variable name=\"foo\" value=\"foo\"/>\n{<caret>}", "foo");
         assertCompletionContains("{f:variable(name: 'buz', value: 'buz')}", "buz");
         assertCompletionContains("{expr -> f:variable(name: 'piped')}", "piped");
     }
