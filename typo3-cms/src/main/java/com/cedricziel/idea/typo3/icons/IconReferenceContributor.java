78385ba116940f86dfd3ef255fcda29462b9ea17
Cedric Ziel <cedric@cedric-ziel.com>
2018-08-13 12:39:47 +0200

    [T3CMS] Contribute IconReferences without PhpIndex exceptions

--- typo3-cms/src/main/java/com/cedricziel/idea/typo3/icons/IconReferenceContributor.java
+++ typo3-cms/src/main/java/com/cedricziel/idea/typo3/icons/IconReferenceContributor.java
@@ -3,6 +3,7 @@ package com.cedricziel.idea.typo3.icons;
 import com.cedricziel.idea.typo3.util.PhpLangUtil;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.php.PhpIndex;
 import com.jetbrains.php.lang.psi.elements.MethodReference;
@@ -21,51 +22,43 @@ public class IconReferenceContributor extends PsiReferenceContributor {
     public void registerReferenceProviders(@NotNull PsiReferenceRegistrar registrar) {
         // known method calls
         registrar.registerReferenceProvider(
-                PlatformPatterns.psiElement(StringLiteralExpression.class),
-                new PsiReferenceProvider() {
-                    @NotNull
-                    @Override
-                    public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
-                        StringLiteralExpression stringLiteralExpression = (StringLiteralExpression) element;
+            PlatformPatterns.psiElement(StringLiteralExpression.class).withSuperParent(2, PlatformPatterns.psiElement(MethodReference.class)),
+            new PsiReferenceProvider() {
+                @NotNull
+                @Override
+                public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+                    StringLiteralExpression stringLiteralExpression = (StringLiteralExpression) element;
+                    MethodReference methodReference = (MethodReference) PsiTreeUtil.findFirstParent(stringLiteralExpression, p -> p instanceof MethodReference);
 
-                        PsiElement parent = stringLiteralExpression.getParent();
-                        while (!(parent instanceof MethodReference)) {
-
-                            if (parent != null) {
-                                parent = parent.getParent();
-
-                                continue;
-                            }
-
-                            return new PsiReference[0];
-                        }
-
-                        MethodReference methodReference = (MethodReference) parent;
-                        String methodName = methodReference.getName();
-
-                        if (methodReference.getFirstPsiChild() instanceof Variable) {
-                            Variable variable = (Variable) methodReference.getFirstPsiChild();
-                            String signature = variable.getSignature();
-                            Collection<? extends PhpNamedElement> bySignature = PhpIndex.getInstance(element.getProject()).getBySignature(signature);
-                            try {
-                                for (PhpNamedElement el : bySignature) {
-                                    if (el.getFQN().equals(ICON_FACTORY) && methodName.equals("getIcon")) {
-                                        return new PsiReference[]{new IconReference(stringLiteralExpression)};
-                                    }
-                                }
-                            } catch (RuntimeException e) {
-                                // invalid index signature, skip
-                            }
-                        }
-
-                        String className = PhpLangUtil.getClassName(stringLiteralExpression);
-                        if (methodName != null && className != null && methodName.equals("getIcon") && className.equals(ICON_FACTORY)) {
-                            return new PsiReference[]{new IconReference(stringLiteralExpression)};
-                        }
-
-                        return new PsiReference[0];
+                    if (methodReference == null) {
+                        return PsiReference.EMPTY_ARRAY;
                     }
+
+                    String methodName = methodReference.getName();
+                    if (methodReference.getFirstPsiChild() instanceof Variable) {
+                        Variable variable = (Variable) methodReference.getFirstPsiChild();
+                        String signature = variable.getSignature();
+                        try {
+                            Collection<? extends PhpNamedElement> bySignature = PhpIndex.getInstance(element.getProject()).getBySignature(signature);
+                            for (PhpNamedElement el : bySignature) {
+                                String fqn = el.getFQN();
+                                if (fqn.equals(ICON_FACTORY) && methodName.equals("getIcon")) {
+                                    return new PsiReference[]{new IconReference(stringLiteralExpression)};
+                                }
+                            }
+                        } catch (RuntimeException e) {
+                            // invalid index signature, skip
+                        }
+                    }
+
+                    String className = PhpLangUtil.getClassName(stringLiteralExpression);
+                    if (methodName != null && className != null && methodName.equals("getIcon") && className.equals(ICON_FACTORY)) {
+                        return new PsiReference[]{new IconReference(stringLiteralExpression)};
+                    }
+
+                    return PsiReference.EMPTY_ARRAY;
                 }
+            }
         );
     }
 }
