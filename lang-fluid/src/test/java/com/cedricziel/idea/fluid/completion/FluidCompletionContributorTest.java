31a320b2b2d14c7efa6a6f3dd219b08d0cb96fdc
Cedric Ziel <cedric@cedric-ziel.com>
2018-08-24 19:06:10 +0200

    [Fluid] Move completion tests to Abstract Test

--- lang-fluid/src/test/java/com/cedricziel/idea/fluid/completion/FluidCompletionContributorTest.java
+++ lang-fluid/src/test/java/com/cedricziel/idea/fluid/completion/FluidCompletionContributorTest.java
@@ -1,7 +1,6 @@
 package com.cedricziel.idea.fluid.completion;
 
 import com.cedricziel.idea.fluid.AbstractFluidTest;
-import com.cedricziel.idea.fluid.lang.FluidFileType;
 
 public class FluidCompletionContributorTest extends AbstractFluidTest {
     @Override
@@ -12,53 +11,15 @@ public class FluidCompletionContributorTest extends AbstractFluidTest {
     public void testInlineViewHelpersAreCompleted() {
         myFixture.copyFileToProject("classes.php");
 
-        myFixture.configureByText(
-            FluidFileType.INSTANCE,
-            "{namespace a=App\\ViewHelpers}\n" +
-                "{ <caret> }"
-        );
-
-        myFixture.completeBasic();
-
-        assertNotNull(myFixture.getLookupElementStrings());
-        assertContainsElements(myFixture.getLookupElementStrings(), "f:alias", "f:base", "f:for");
-        assertContainsElements(myFixture.getLookupElementStrings(), "a:foo");
+        assertLookupStringOnFluidCaret("{namespace a=App\\ViewHelpers}\n{ <caret> }", "f:alias", "f:base", "f:for");
+        assertLookupStringOnFluidCaret("{namespace a=App\\ViewHelpers}\n{ <caret> }", "a:foo");
     }
 
     public void testInlineArgumentsAreCompleted() {
         myFixture.copyFileToProject("classes.php");
 
-        myFixture.configureByText(
-            FluidFileType.INSTANCE,
-            "{namespace a=App\\ViewHelpers}\n" +
-                "{ a:foo(<caret>) }"
-        );
-
-        myFixture.completeBasic();
-
-        assertNotNull(myFixture.getLookupElementStrings());
-        assertContainsElements(myFixture.getLookupElementStrings(), "uid");
-
-        myFixture.configureByText(
-            FluidFileType.INSTANCE,
-            "{namespace a=App\\ViewHelpers}\n" +
-                "{ a:foo(u<caret>) }"
-        );
-
-        myFixture.completeBasic();
-
-        assertNotNull(myFixture.getLookupElementStrings());
-        assertContainsElements(myFixture.getLookupElementStrings(), "uid");
-
-        myFixture.configureByText(
-            FluidFileType.INSTANCE,
-            "{namespace a=App\\ViewHelpers}\n" +
-                "{ a:foo(u<caret>:) }"
-        );
-
-        myFixture.completeBasic();
-
-        assertNotNull(myFixture.getLookupElementStrings());
-        assertContainsElements(myFixture.getLookupElementStrings(), "uid");
+        assertLookupStringOnFluidCaret("{namespace a=App\\ViewHelpers}\n{ a:foo(<caret>) }", "uid");
+        assertLookupStringOnFluidCaret("{namespace a=App\\ViewHelpers}\n { a:foo(u<caret>) }", "uid");
+        assertLookupStringOnFluidCaret("{namespace a=App\\ViewHelpers}\n{ a:foo(u<caret>:) }", "uid");
     }
 }
