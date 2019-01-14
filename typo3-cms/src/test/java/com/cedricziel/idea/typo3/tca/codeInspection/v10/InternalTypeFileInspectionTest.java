211f837983b92ba186e68374bbdac19968be3b93
Cedric Ziel <cedric@cedric-ziel.com>
2019-01-14 22:04:47 +0100

    [T3CMS] Add Inspection for invalid internal_type TCA option on v9 and up

---
+++ typo3-cms/src/test/java/com/cedricziel/idea/typo3/tca/codeInspection/v10/InternalTypeFileInspectionTest.java
@@ -0,0 +1,58 @@
+package com.cedricziel.idea.typo3.tca.codeInspection.v10;
+
+import com.cedricziel.idea.typo3.AbstractTestCase;
+import com.cedricziel.idea.typo3.tca.codeInspection.InvalidQuantityInspection;
+import com.intellij.openapi.vfs.VirtualFile;
+
+public class InternalTypeFileInspectionTest extends AbstractTestCase {
+    @Override
+    protected String getTestDataPath() {
+        return super.getTestDataPath() + "/tca/codeInspection/v10";
+    }
+
+    public void testInvalidInternalTypeIsNotDisplayedWhenPluginIsDisabled() {
+        disablePlugin();
+
+        myFixture.enableInspections(InternalTypeFileInspection.class);
+
+        myFixture.addFileToProject("foo/ext_emconf.php", "");
+
+        VirtualFile virtualFile = myFixture.copyFileToProject("invalid_internal_type_disabled.php", "foo/Configuration/TCA/tx_news_domain_model_link.php");
+
+        myFixture.configureFromExistingVirtualFile(virtualFile);
+        myFixture.checkHighlighting();
+    }
+
+    public void testInvalidInternalTypeIsNotDisplayedOnv8() {
+        myFixture.enableInspections(InternalTypeFileInspection.class);
+
+        myFixture.addFileToProject("foo/ext_emconf.php", "");
+
+        VirtualFile virtualFile = myFixture.copyFileToProject("invalid_internal_type_v8.php", "foo/Configuration/TCA/tx_news_domain_model_link.php");
+
+        myFixture.configureFromExistingVirtualFile(virtualFile);
+        myFixture.checkHighlighting();
+    }
+
+    public void testInvalidInternalTypeIsDisplayedOnv9() {
+        myFixture.enableInspections(InternalTypeFileInspection.class);
+
+        myFixture.addFileToProject("foo/ext_emconf.php", "");
+
+        VirtualFile virtualFile = myFixture.copyFileToProject("invalid_internal_type_v9.php", "foo/Configuration/TCA/tx_news_domain_model_link.php");
+
+        myFixture.configureFromExistingVirtualFile(virtualFile);
+        myFixture.checkHighlighting();
+    }
+
+    public void testInvalidInternalTypeIsHighlightedOnv10() {
+        myFixture.enableInspections(InternalTypeFileInspection.class);
+
+        myFixture.addFileToProject("foo/ext_emconf.php", "");
+
+        VirtualFile virtualFile = myFixture.copyFileToProject("invalid_internal_type_v10.php", "foo/Configuration/TCA/tx_news_domain_model_link.php");
+
+        myFixture.configureFromExistingVirtualFile(virtualFile);
+        myFixture.checkHighlighting();
+    }
+}
