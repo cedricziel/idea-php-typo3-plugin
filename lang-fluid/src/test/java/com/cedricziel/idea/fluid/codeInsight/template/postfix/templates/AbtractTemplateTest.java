e7b7ce5863339a4d404d131102b3bc3fe9347143
Cedric Ziel <cedric@cedric-ziel.com>
2019-07-11 00:12:36 +0200

    [FLUID] Re-add tests after platform upgrade

---
+++ lang-fluid/src/test/java/com/cedricziel/idea/fluid/codeInsight/template/postfix/templates/AbtractTemplateTest.java
@@ -0,0 +1,61 @@
+package com.cedricziel.idea.fluid.codeInsight.template.postfix.templates;
+
+import com.intellij.codeInsight.completion.CompletionAutoPopupTestCase;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.impl.LookupImpl;
+import com.intellij.codeInsight.template.impl.LiveTemplateCompletionContributor;
+import com.intellij.codeInsight.template.postfix.completion.PostfixTemplateLookupElement;
+import com.intellij.codeInsight.template.postfix.settings.PostfixTemplatesSettings;
+import com.intellij.testFramework.EdtTestUtil;
+import com.intellij.util.containers.ContainerUtil;
+
+abstract public class AbtractTemplateTest extends CompletionAutoPopupTestCase {
+    @Override
+    public void setUp() throws Exception {
+
+        super.setUp();
+
+        LiveTemplateCompletionContributor.setShowTemplatesInTests(false, myFixture.getTestRootDisposable());
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        try {
+            PostfixTemplatesSettings settings = PostfixTemplatesSettings.getInstance();
+            assertNotNull(settings);
+            settings.setProviderToDisabledTemplates(ContainerUtil.newHashMap());
+            settings.setPostfixTemplatesEnabled(true);
+            settings.setTemplatesCompletionEnabled(true);
+        } finally {
+            super.tearDown();
+        }
+    }
+
+    protected void doCompleteTest(String textToType, char c) {
+        configureByFile();
+        type(textToType);
+        assertNotNull(getLookup());
+        myFixture.type(c);
+        checkResultByFile();
+    }
+
+    private void configureByFile() {
+        EdtTestUtil.runInEdtAndWait(() -> myFixture.configureByFile(getTestName(true) + ".fluid"));
+    }
+
+    private void checkResultByFile() {
+        myFixture.checkResultByFile(getTestName(true) + "_after.fluid", true);
+    }
+
+    protected void testLiveTemplateIsAvailable(String completionChar, String templateContent, Class templateClass) {
+        myFixture.configureByText("foo.fluid", templateContent);
+        type(completionChar);
+        LookupImpl lookup = getLookup();
+        assertNotNull(lookup);
+
+        LookupElement item = lookup.getCurrentItem();
+        assertNotNull(item);
+        assertInstanceOf(item, PostfixTemplateLookupElement.class);
+        assertInstanceOf(((PostfixTemplateLookupElement) item).getPostfixTemplate(), templateClass);
+    }
+}
