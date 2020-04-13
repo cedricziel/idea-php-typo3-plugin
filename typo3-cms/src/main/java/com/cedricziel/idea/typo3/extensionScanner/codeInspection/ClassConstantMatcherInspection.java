f547885446592f01cc9b052288f70c95909b041e
Cedric Ziel <cedric@cedric-ziel.com>
2020-04-13 19:09:08 +0200

    [T3CMS] Migrate ClassConstantMatcherInspection to use DeprecationUtility

--- typo3-cms/src/main/java/com/cedricziel/idea/typo3/extensionScanner/codeInspection/ClassConstantMatcherInspection.java
+++ typo3-cms/src/main/java/com/cedricziel/idea/typo3/extensionScanner/codeInspection/ClassConstantMatcherInspection.java
@@ -1,29 +1,16 @@
 package com.cedricziel.idea.typo3.extensionScanner.codeInspection;
 
+import com.cedricziel.idea.typo3.codeInspection.PluginEnabledPhpInspection;
+import com.cedricziel.idea.typo3.util.DeprecationUtility;
 import com.intellij.codeInsight.daemon.GroupNames;
 import com.intellij.codeInspection.ProblemsHolder;
-import com.intellij.patterns.PlatformPatterns;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.search.FilenameIndex;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.util.PsiTreeUtil;
-import com.jetbrains.php.lang.inspections.PhpInspection;
-import com.jetbrains.php.lang.parser.PhpElementTypes;
 import com.jetbrains.php.lang.psi.elements.ClassConstantReference;
-import com.jetbrains.php.lang.psi.elements.PhpPsiElement;
-import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
 import com.jetbrains.php.lang.psi.visitors.PhpElementVisitor;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-public class ClassConstantMatcherInspection extends PhpInspection {
+public class ClassConstantMatcherInspection extends PluginEnabledPhpInspection {
     @Nls
     @NotNull
     @Override
@@ -38,45 +25,18 @@ public class ClassConstantMatcherInspection extends PhpInspection {
 
     @NotNull
     @Override
-    public PsiElementVisitor buildVisitor(@NotNull ProblemsHolder problemsHolder, boolean b) {
+    public PsiElementVisitor buildRealVisitor(@NotNull ProblemsHolder problemsHolder, boolean b) {
         return new PhpElementVisitor() {
+
             @Override
-            public void visitPhpElement(PhpPsiElement element) {
-
-                if (!PlatformPatterns.psiElement(PhpElementTypes.CLASS_CONSTANT_REFERENCE).accepts(element)) {
-                    return;
+            public void visitPhpClassConstantReference(ClassConstantReference constantReference) {
+                if (DeprecationUtility.isDeprecated(problemsHolder.getProject(), constantReference)) {
+                    problemsHolder.registerProblem(constantReference, "Deprecated class constant");
                 }
 
-                Set<String> constants = getDeprecatedClassConstants(element);
-                ClassConstantReference classConstantReference = (ClassConstantReference) element;
-                if (constants.contains(classConstantReference.getText())) {
-                    problemsHolder.registerProblem(element, "Deprecated class constant");
-                }
+                super.visitPhpClassConstantReference(constantReference);
             }
         };
     }
 
-    private Set<String> getDeprecatedClassConstants(PhpPsiElement element) {
-        Set<PsiElement> elements = new HashSet<>();
-        PsiFile[] classConstantMatcherFiles = FilenameIndex.getFilesByName(element.getProject(), "ClassConstantMatcher.php", GlobalSearchScope.allScope(element.getProject()));
-        for (PsiFile file : classConstantMatcherFiles) {
-
-            Collections.addAll(
-                    elements,
-                    PsiTreeUtil.collectElements(file, el -> PlatformPatterns
-                            .psiElement(StringLiteralExpression.class)
-                            .withParent(
-                                    PlatformPatterns.psiElement(PhpElementTypes.ARRAY_KEY)
-                                            .withAncestor(
-                                                    4,
-                                                    PlatformPatterns.psiElement(PhpElementTypes.RETURN)
-                                            )
-                            )
-                            .accepts(el)
-                    )
-            );
-        }
-
-        return elements.stream().map(stringLiteral -> ((StringLiteralExpression)stringLiteral).getContents()).collect(Collectors.toSet());
-    }
 }
