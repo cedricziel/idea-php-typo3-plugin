f3b41833addb7c787d89a705ba3df9ca74490046
Cedric Ziel <cedric@cedric-ziel.com>
2020-04-28 14:08:19 +0200

    [T3CMS] Inject Languages into asset viewhelpers

---
+++ typo3-cms/src/main/java/com/cedricziel/idea/typo3/languageInjection/CssInjector.java
@@ -0,0 +1,41 @@
+package com.cedricziel.idea.typo3.languageInjection;
+
+import com.intellij.lang.css.CSSLanguage;
+import com.intellij.lang.javascript.JavascriptLanguage;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.patterns.PsiElementPattern;
+import com.intellij.patterns.XmlPatterns;
+import com.intellij.psi.InjectedLanguagePlaces;
+import com.intellij.psi.LanguageInjector;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiLanguageInjectionHost;
+import com.intellij.psi.xml.XmlElementType;
+import org.jetbrains.annotations.NotNull;
+
+public class CssInjector implements LanguageInjector {
+    @Override
+    public void getLanguagesToInject(@NotNull PsiLanguageInjectionHost host, @NotNull InjectedLanguagePlaces injectionPlacesRegistrar) {
+        if (getXmlFAssetCssContentPattern().accepts(host)) {
+            injectionPlacesRegistrar.addPlace(CSSLanguage.INSTANCE, new TextRange(0, host.getTextLength()-1), null, null);
+        };
+
+        if (getXmlFAssetJsContentPattern().accepts(host)) {
+            injectionPlacesRegistrar.addPlace(JavascriptLanguage.INSTANCE, new TextRange(0, host.getTextLength()-1), null, null);
+        };
+    }
+
+    @NotNull
+    private PsiElementPattern.Capture<PsiElement> getXmlFAssetCssContentPattern() {
+        return PlatformPatterns.psiElement(XmlElementType.XML_TEXT).withParent(
+            XmlPatterns.xmlTag().withName("f:asset.css")
+        );
+    }
+
+    @NotNull
+    private PsiElementPattern.Capture<PsiElement> getXmlFAssetJsContentPattern() {
+        return PlatformPatterns.psiElement(XmlElementType.XML_TEXT).withParent(
+            XmlPatterns.xmlTag().withName("f:asset.script")
+        );
+    }
+}
