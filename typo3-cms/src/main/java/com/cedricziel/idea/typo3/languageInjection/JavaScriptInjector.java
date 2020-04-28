6b3bb3c645504e5968cab843640fd362e2250775
Cedric Ziel <cedric@cedric-ziel.com>
2020-04-28 18:04:33 +0200

    [T3CMS] Use additional config files for css and javascript

--- typo3-cms/src/main/java/com/cedricziel/idea/typo3/languageInjection/JavaScriptInjector.java
+++ typo3-cms/src/main/java/com/cedricziel/idea/typo3/languageInjection/JavaScriptInjector.java
@@ -1,6 +1,5 @@
 package com.cedricziel.idea.typo3.languageInjection;
 
-import com.intellij.lang.css.CSSLanguage;
 import com.intellij.lang.javascript.JavascriptLanguage;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.patterns.PlatformPatterns;
@@ -13,18 +12,18 @@ import com.intellij.psi.PsiLanguageInjectionHost;
 import com.intellij.psi.xml.XmlElementType;
 import org.jetbrains.annotations.NotNull;
 
-public class CssInjector implements LanguageInjector {
+public class JavaScriptInjector implements LanguageInjector {
     @Override
     public void getLanguagesToInject(@NotNull PsiLanguageInjectionHost host, @NotNull InjectedLanguagePlaces injectionPlacesRegistrar) {
-        if (getXmlFAssetCssContentPattern().accepts(host)) {
-            injectionPlacesRegistrar.addPlace(CSSLanguage.INSTANCE, new TextRange(0, host.getTextLength()-1), null, null);
+        if (getXmlFAssetJsContentPattern().accepts(host)) {
+            injectionPlacesRegistrar.addPlace(JavascriptLanguage.INSTANCE, new TextRange(0, host.getTextLength()-1), null, null);
         };
     }
 
     @NotNull
-    private PsiElementPattern.Capture<PsiElement> getXmlFAssetCssContentPattern() {
+    private PsiElementPattern.Capture<PsiElement> getXmlFAssetJsContentPattern() {
         return PlatformPatterns.psiElement(XmlElementType.XML_TEXT).withParent(
-            XmlPatterns.xmlTag().withName("f:asset.css")
+            XmlPatterns.xmlTag().withName("f:asset.script")
         );
     }
 }
