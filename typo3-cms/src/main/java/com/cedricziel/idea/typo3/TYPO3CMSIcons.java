698f523cd74ba06e96b5ec497b32ae23215f8d06
Cedric Ziel <cedric@cedric-ziel.com>
2022-02-19 13:43:54 +0100

    Migrate deprecated getIcon method usage

--- typo3-cms/src/main/java/com/cedricziel/idea/typo3/TYPO3CMSIcons.java
+++ typo3-cms/src/main/java/com/cedricziel/idea/typo3/TYPO3CMSIcons.java
@@ -5,10 +5,10 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface TYPO3CMSIcons {
-    Icon TYPO3_ICON = IconLoader.getIcon("/icons/dist/icon-typo3.png");
-    Icon ROUTE_ICON = IconLoader.getIcon("/icons/dist/icon-navigate-route-definition.png");
-    Icon ICON_NOT_RESOLVED = IconLoader.getIcon("/icons/dist/icon-icon-not-resolved.png");
-    Icon SIGNAL_COMPLETED_METHOD = IconLoader.getIcon("/icons/dist/icon-typo3.png");
-    Icon CONTEXT_ASPECT_ICON = IconLoader.getIcon("/icons/dist/icon-typo3.png");
-    Icon SITE_CONFIG_INDEX = IconLoader.getIcon("/icons/dist/icon-typo3.png");
+    Icon TYPO3_ICON = IconLoader.getIcon("/icons/dist/icon-typo3.png", TYPO3CMSIcons.class);
+    Icon ROUTE_ICON = IconLoader.getIcon("/icons/dist/icon-navigate-route-definition.png", TYPO3CMSIcons.class);
+    Icon ICON_NOT_RESOLVED = IconLoader.getIcon("/icons/dist/icon-icon-not-resolved.png", TYPO3CMSIcons.class);
+    Icon SIGNAL_COMPLETED_METHOD = IconLoader.getIcon("/icons/dist/icon-typo3.png", TYPO3CMSIcons.class);
+    Icon CONTEXT_ASPECT_ICON = IconLoader.getIcon("/icons/dist/icon-typo3.png", TYPO3CMSIcons.class);
+    Icon SITE_CONFIG_INDEX = IconLoader.getIcon("/icons/dist/icon-typo3.png", TYPO3CMSIcons.class);
 }
